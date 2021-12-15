3fccacb03cf21e671d640eac9272e6a2892b59ae
batphonghan <vanhungdeveloper@gmail.com>
2021-12-15 21:35:08 +0700

    Add nextGame

--- x/checkers/types/query.pb.go
+++ x/checkers/types/query.pb.go
@@ -11,7 +11,11 @@ import (
 	proto "github.com/gogo/protobuf/proto"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
+	io "io"
 	math "math"
+	math_bits "math/bits"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -25,22 +29,115 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
+// this line is used by starport scaffolding # 3
+type QueryGetNextGameRequest struct {
+}
+
+func (m *QueryGetNextGameRequest) Reset()         { *m = QueryGetNextGameRequest{} }
+func (m *QueryGetNextGameRequest) String() string { return proto.CompactTextString(m) }
+func (*QueryGetNextGameRequest) ProtoMessage()    {}
+func (*QueryGetNextGameRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_3c482788bba85e7a, []int{0}
+}
+func (m *QueryGetNextGameRequest) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryGetNextGameRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryGetNextGameRequest.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryGetNextGameRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryGetNextGameRequest.Merge(m, src)
+}
+func (m *QueryGetNextGameRequest) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryGetNextGameRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryGetNextGameRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryGetNextGameRequest proto.InternalMessageInfo
+
+type QueryGetNextGameResponse struct {
+	NextGame *NextGame `protobuf:"bytes,1,opt,name=NextGame,proto3" json:"NextGame,omitempty"`
+}
+
+func (m *QueryGetNextGameResponse) Reset()         { *m = QueryGetNextGameResponse{} }
+func (m *QueryGetNextGameResponse) String() string { return proto.CompactTextString(m) }
+func (*QueryGetNextGameResponse) ProtoMessage()    {}
+func (*QueryGetNextGameResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_3c482788bba85e7a, []int{1}
+}
+func (m *QueryGetNextGameResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryGetNextGameResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryGetNextGameResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryGetNextGameResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryGetNextGameResponse.Merge(m, src)
+}
+func (m *QueryGetNextGameResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryGetNextGameResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryGetNextGameResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryGetNextGameResponse proto.InternalMessageInfo
+
+func (m *QueryGetNextGameResponse) GetNextGame() *NextGame {
+	if m != nil {
+		return m.NextGame
+	}
+	return nil
+}
+
+func init() {
+	proto.RegisterType((*QueryGetNextGameRequest)(nil), "batphonghan.checkers.checkers.QueryGetNextGameRequest")
+	proto.RegisterType((*QueryGetNextGameResponse)(nil), "batphonghan.checkers.checkers.QueryGetNextGameResponse")
+}
+
 func init() { proto.RegisterFile("checkers/query.proto", fileDescriptor_3c482788bba85e7a) }
 
 var fileDescriptor_3c482788bba85e7a = []byte{
-	// 189 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x4c, 0xce, 0xb1, 0xce, 0x82, 0x30,
-	0x14, 0x05, 0x60, 0x18, 0xfe, 0xdf, 0x84, 0xd1, 0x38, 0x11, 0xed, 0x03, 0x38, 0x70, 0x45, 0xdf,
-	0xc0, 0xd1, 0xcd, 0xd5, 0xed, 0xb6, 0x69, 0xda, 0x46, 0xe9, 0xad, 0xb4, 0x18, 0x79, 0x0b, 0x1f,
-	0xcb, 0x91, 0xd1, 0xd1, 0xc0, 0x8b, 0x18, 0x21, 0xa2, 0xdb, 0x19, 0xbe, 0x73, 0x72, 0x92, 0x99,
-	0xd0, 0x52, 0x1c, 0x65, 0xe9, 0xe1, 0x5c, 0xc9, 0xb2, 0xce, 0x5c, 0x49, 0x81, 0xa6, 0x0b, 0x8e,
-	0xc1, 0x69, 0xb2, 0x4a, 0xa3, 0xcd, 0x3e, 0x62, 0x0c, 0xe9, 0x5c, 0x11, 0xa9, 0x93, 0x04, 0x74,
-	0x06, 0xd0, 0x5a, 0x0a, 0x18, 0x0c, 0x59, 0x3f, 0x94, 0xd3, 0xa5, 0x20, 0x5f, 0x90, 0x07, 0x8e,
-	0x5e, 0x0e, 0xab, 0x70, 0xc9, 0xb9, 0x0c, 0x98, 0x83, 0x43, 0x65, 0x6c, 0x8f, 0x07, 0xbb, 0x9e,
-	0x24, 0x7f, 0xfb, 0xb7, 0xd8, 0xee, 0xee, 0x2d, 0x8b, 0x9b, 0x96, 0xc5, 0xcf, 0x96, 0xc5, 0xb7,
-	0x8e, 0x45, 0x4d, 0xc7, 0xa2, 0x47, 0xc7, 0xa2, 0xc3, 0x4a, 0x99, 0xa0, 0x2b, 0x9e, 0x09, 0x2a,
-	0xe0, 0xe7, 0x16, 0x8c, 0xc7, 0xaf, 0xdf, 0x18, 0x6a, 0x27, 0x3d, 0xff, 0xef, 0xb7, 0x37, 0xaf,
-	0x00, 0x00, 0x00, 0xff, 0xff, 0xc8, 0x17, 0x8c, 0x2d, 0xdc, 0x00, 0x00, 0x00,
+	// 299 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x49, 0xce, 0x48, 0x4d,
+	0xce, 0x4e, 0x2d, 0x2a, 0xd6, 0x2f, 0x2c, 0x4d, 0x2d, 0xaa, 0xd4, 0x2b, 0x28, 0xca, 0x2f, 0xc9,
+	0x17, 0x92, 0x4d, 0x4a, 0x2c, 0x29, 0xc8, 0xc8, 0xcf, 0x4b, 0xcf, 0x48, 0xcc, 0xd3, 0x83, 0xa9,
+	0x80, 0x33, 0xa4, 0x64, 0xd2, 0xf3, 0xf3, 0xd3, 0x73, 0x52, 0xf5, 0x13, 0x0b, 0x32, 0xf5, 0x13,
+	0xf3, 0xf2, 0xf2, 0x4b, 0x12, 0x4b, 0x32, 0xf3, 0xf3, 0x8a, 0x21, 0x9a, 0xa5, 0xb4, 0x92, 0xf3,
+	0x8b, 0x73, 0xf3, 0x8b, 0xf5, 0x93, 0x12, 0x8b, 0x53, 0x21, 0xa6, 0xea, 0x97, 0x19, 0x26, 0xa5,
+	0x96, 0x24, 0x1a, 0xea, 0x17, 0x24, 0xa6, 0x67, 0xe6, 0x81, 0x15, 0x43, 0xd5, 0x4a, 0xc0, 0xad,
+	0xcf, 0x4b, 0xad, 0x28, 0x89, 0x4f, 0x4f, 0xcc, 0x4d, 0x85, 0xc8, 0x28, 0x49, 0x72, 0x89, 0x07,
+	0x82, 0xf4, 0xba, 0xa7, 0x96, 0xf8, 0xa5, 0x56, 0x94, 0xb8, 0x27, 0xe6, 0xa6, 0x06, 0xa5, 0x16,
+	0x96, 0xa6, 0x16, 0x97, 0x28, 0xc5, 0x73, 0x49, 0x60, 0x4a, 0x15, 0x17, 0xe4, 0xe7, 0x15, 0xa7,
+	0x0a, 0x39, 0x73, 0x71, 0xc0, 0xc4, 0x24, 0x18, 0x15, 0x18, 0x35, 0xb8, 0x8d, 0xd4, 0xf5, 0xf0,
+	0x7a, 0x46, 0x0f, 0x6e, 0x04, 0x5c, 0xa3, 0xd1, 0x36, 0x46, 0x2e, 0x56, 0xb0, 0x0d, 0x42, 0x6b,
+	0x18, 0x11, 0xe6, 0x09, 0x99, 0x11, 0x30, 0x09, 0x87, 0x7b, 0xa5, 0xcc, 0x49, 0xd6, 0x07, 0xf1,
+	0x8c, 0x92, 0x7e, 0xd3, 0xe5, 0x27, 0x93, 0x99, 0x34, 0x85, 0xd4, 0xf5, 0x91, 0x0c, 0xd0, 0x87,
+	0x07, 0x19, 0x4a, 0xd8, 0x81, 0x34, 0x3a, 0x79, 0x9d, 0x78, 0x24, 0xc7, 0x78, 0xe1, 0x91, 0x1c,
+	0xe3, 0x83, 0x47, 0x72, 0x8c, 0x13, 0x1e, 0xcb, 0x31, 0x5c, 0x78, 0x2c, 0xc7, 0x70, 0xe3, 0xb1,
+	0x1c, 0x43, 0x94, 0x41, 0x7a, 0x66, 0x49, 0x46, 0x69, 0x92, 0x5e, 0x72, 0x7e, 0x2e, 0x76, 0xc3,
+	0x2a, 0x10, 0xcc, 0x92, 0xca, 0x82, 0xd4, 0xe2, 0x24, 0x36, 0x70, 0x3c, 0x18, 0x03, 0x02, 0x00,
+	0x00, 0xff, 0xff, 0x63, 0x21, 0xf7, 0x19, 0x22, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -55,6 +152,8 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type QueryClient interface {
+	// Queries a nextGame by index.
+	NextGame(ctx context.Context, in *QueryGetNextGameRequest, opts ...grpc.CallOption) (*QueryGetNextGameResponse, error)
 }
 
 type queryClient struct {
@@ -65,22 +164,378 @@ func NewQueryClient(cc grpc1.ClientConn) QueryClient {
 	return &queryClient{cc}
 }
 
+func (c *queryClient) NextGame(ctx context.Context, in *QueryGetNextGameRequest, opts ...grpc.CallOption) (*QueryGetNextGameResponse, error) {
+	out := new(QueryGetNextGameResponse)
+	err := c.cc.Invoke(ctx, "/batphonghan.checkers.checkers.Query/NextGame", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // QueryServer is the server API for Query service.
 type QueryServer interface {
+	// Queries a nextGame by index.
+	NextGame(context.Context, *QueryGetNextGameRequest) (*QueryGetNextGameResponse, error)
 }
 
 // UnimplementedQueryServer can be embedded to have forward compatible implementations.
 type UnimplementedQueryServer struct {
 }
 
+func (*UnimplementedQueryServer) NextGame(ctx context.Context, req *QueryGetNextGameRequest) (*QueryGetNextGameResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method NextGame not implemented")
+}
+
 func RegisterQueryServer(s grpc1.Server, srv QueryServer) {
 	s.RegisterService(&_Query_serviceDesc, srv)
+}
+
+func _Query_NextGame_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(QueryGetNextGameRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(QueryServer).NextGame(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/batphonghan.checkers.checkers.Query/NextGame",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(QueryServer).NextGame(ctx, req.(*QueryGetNextGameRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "batphonghan.checkers.checkers.Query",
 	HandlerType: (*QueryServer)(nil),
-	Methods:     []grpc.MethodDesc{},
-	Streams:     []grpc.StreamDesc{},
-	Metadata:    "checkers/query.proto",
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "NextGame",
+			Handler:    _Query_NextGame_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "checkers/query.proto",
 }
+
+func (m *QueryGetNextGameRequest) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryGetNextGameRequest) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryGetNextGameRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
+func (m *QueryGetNextGameResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryGetNextGameResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryGetNextGameResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.NextGame != nil {
+		{
+			size, err := m.NextGame.MarshalToSizedBuffer(dAtA[:i])
+			if err != nil {
+				return 0, err
+			}
+			i -= size
+			i = encodeVarintQuery(dAtA, i, uint64(size))
+		}
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func encodeVarintQuery(dAtA []byte, offset int, v uint64) int {
+	offset -= sovQuery(v)
+	base := offset
+	for v >= 1<<7 {
+		dAtA[offset] = uint8(v&0x7f | 0x80)
+		v >>= 7
+		offset++
+	}
+	dAtA[offset] = uint8(v)
+	return base
+}
+func (m *QueryGetNextGameRequest) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *QueryGetNextGameResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.NextGame != nil {
+		l = m.NextGame.Size()
+		n += 1 + l + sovQuery(uint64(l))
+	}
+	return n
+}
+
+func sovQuery(x uint64) (n int) {
+	return (math_bits.Len64(x|1) + 6) / 7
+}
+func sozQuery(x uint64) (n int) {
+	return sovQuery(uint64((x << 1) ^ uint64((int64(x) >> 63))))
+}
+func (m *QueryGetNextGameRequest) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryGetNextGameRequest: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryGetNextGameRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *QueryGetNextGameResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryGetNextGameResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryGetNextGameResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field NextGame", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.NextGame == nil {
+				m.NextGame = &NextGame{}
+			}
+			if err := m.NextGame.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func skipQuery(dAtA []byte) (n int, err error) {
+	l := len(dAtA)
+	iNdEx := 0
+	depth := 0
+	for iNdEx < l {
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return 0, ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return 0, io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= (uint64(b) & 0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		wireType := int(wire & 0x7)
+		switch wireType {
+		case 0:
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return 0, ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return 0, io.ErrUnexpectedEOF
+				}
+				iNdEx++
+				if dAtA[iNdEx-1] < 0x80 {
+					break
+				}
+			}
+		case 1:
+			iNdEx += 8
+		case 2:
+			var length int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return 0, ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return 0, io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				length |= (int(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if length < 0 {
+				return 0, ErrInvalidLengthQuery
+			}
+			iNdEx += length
+		case 3:
+			depth++
+		case 4:
+			if depth == 0 {
+				return 0, ErrUnexpectedEndOfGroupQuery
+			}
+			depth--
+		case 5:
+			iNdEx += 4
+		default:
+			return 0, fmt.Errorf("proto: illegal wireType %d", wireType)
+		}
+		if iNdEx < 0 {
+			return 0, ErrInvalidLengthQuery
+		}
+		if depth == 0 {
+			return iNdEx, nil
+		}
+	}
+	return 0, io.ErrUnexpectedEOF
+}
+
+var (
+	ErrInvalidLengthQuery        = fmt.Errorf("proto: negative length found during unmarshaling")
+	ErrIntOverflowQuery          = fmt.Errorf("proto: integer overflow")
+	ErrUnexpectedEndOfGroupQuery = fmt.Errorf("proto: unexpected end of group")
+)
